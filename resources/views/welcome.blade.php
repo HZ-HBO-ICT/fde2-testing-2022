809875b4e6a0f335475423349b8a49790aed4e6d
Frans Blauw <frans.blauw@hz.nl>
2022-05-12 16:56:36 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>My Awesome Course Tracker</title>
+</head>
+<body>
+<h1>Welcome to my Course Tracker</h1>
+<a href="{{ route("courses.index") }}">Go to Courses</a>
+</body>
+</html>
